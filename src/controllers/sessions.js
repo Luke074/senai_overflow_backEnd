88e972d5c8a28c5f7d1bc55fbabe12fd6e9e50ed
Luke074 <mendeslucas277@gmail.com>
2021-02-09 16:01:15 -0300

    colocando um TimeOut para a api responder

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -23,15 +23,21 @@ module.exports = {
                 studentName: student.name,
             });
 
-            res.status(201).send({
-                student: {
-                    studentId: student.id,
-                    name: student.name,
-                    ra: student.ra,
-                    email: student.email
-                },
-                token
-            });
+            setTimeout(
+                () => {
+                    res.status(201).send({
+                        student: {
+                            studentId: student.id,
+                            name: student.name,
+                            ra: student.ra,
+                            email: student.email
+                        },
+                        token
+                    });
+                }, 2000
+            );
+
+
 
         } catch (error) {
             console.log(error);
